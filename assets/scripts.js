f55ed5b05b08da009c1c2f625d9ea7dd75bce693
Marcin <cineqpl@yahoo.com>
2019-03-05 23:49:54 +0100

    main menu, jquery scrolls

--- assets/scripts.js
+++ assets/scripts.js
@@ -9,5 +9,18 @@ $(document).ready( function() {
         window.status = $(this).find("a:first").attr("href");
     }, function() {
         window.status = "";
+    }),
+
+    $("#projectslink").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 500)
     });
+
+    $("#mymedialink").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#mymedia").offset().top
+        }, 800)
+    });
+
 });
