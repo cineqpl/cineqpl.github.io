88f2c09ce792712fc0db301b0ac2c49626397d4c
Marcin <cineqpl@yahoo.com>
2019-03-10 22:36:26 +0100

    height bugfix, main title animation added

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready( function() {
 
+    $("#bloglink").click(function () {
+        window.open($(this).find("a:first").attr("href"));
+        return false;
+    });
+
     $(".project").click(function() {
         window.open($(this).find("a:first").attr("href"));
         return false;
@@ -23,6 +28,11 @@ $(document).ready( function() {
         }, 800)
     });
 
+    $("#bloglink").click(function () {
+        window.open($(this).find("a:first").attr("href"));
+        return false;
+    });
+
     $("#mymedialink").click(function () {
         $('html, body').animate({
             scrollTop: $("#mymedia").offset().top
@@ -30,3 +40,40 @@ $(document).ready( function() {
     });
 
 });
+
+window.onload = function() {
+
+    var fullTitle1 = document.getElementById("maintitle1").innerHTML;
+    var fullTitle2 = document.getElementById("maintitle2").innerHTML;
+
+    var loadTitle1 = "";
+    var loadTitle2 = "";
+
+    var triggerSecond = 70 * fullTitle1.length;
+
+    document.getElementById("maintitle1").innerHTML = loadTitle1;
+    document.getElementById("maintitle2").innerHTML = loadTitle2;
+
+    function doSetTimeout1(i) {
+        setTimeout(function () {
+            loadTitle1 += fullTitle1.charAt(i);
+            document.getElementById("maintitle1").innerHTML = loadTitle1;
+        }, i*70);
+    }
+
+    for(let i = 0; i < fullTitle1.length; i++) {
+        doSetTimeout1(i);
+    }
+
+    function doSetTimeout2(i) {
+        setTimeout(function () {
+            loadTitle2 += fullTitle2.charAt(i);
+            document.getElementById("maintitle2").innerHTML = loadTitle2;
+        }, triggerSecond + i*35);
+    }
+
+    for(let i = 0; i < fullTitle2.length; i++) {
+        doSetTimeout2(i);
+    }
+
+}
